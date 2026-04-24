da46a8ebc93eaa7571c2557fca58fc2ae617b2b2
Nicco Kunzmann <niccokunzmann@rambler.ru>
2026-04-24 19:54:28 +0100

    use importlib because of setuptools not shipping pkg_resources in later versions

--- pinax/announcements/__init__.py
+++ pinax/announcements/__init__.py
@@ -1,3 +1,6 @@
-import pkg_resources
-
-__version__ = pkg_resources.get_distribution("pinax-announcements").version
+try:
+    from importlib.metadata import version
+    __version__ = version("pinax-announcements")
+except ImportError:
+    import pkg_resources
+    __version__ = pkg_resources.get_distribution("pinax-announcements").version
